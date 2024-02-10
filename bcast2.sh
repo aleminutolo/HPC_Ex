55772243c6a9838f15d6c32257c44b7d8e1cb8a5
Alessandro Minutolo <98023987+aleminutolo@users.noreply.github.com>
2024-02-10 14:46:49 +0100

    Update bcast2.sh

--- bcast2.sh
+++ bcast2.sh
@@ -9,7 +9,7 @@
 
 module load openMPI/4.1.5/gnu/12.2.1
 
-echo "Processes,Size,Latency" > output_processes_size_2_new.csv
+echo "Processes,Size,Latency" > output_processes_size_gather_2_new.csv
 
 # Numero di ripetizioni per ottenere una media
 repetitions=10000
@@ -24,10 +24,10 @@ do
         size=$((2**size_power))
 
         # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-        result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 2 osu_bcast -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
+        result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_gather_algorithm 2 osu_gather -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
 	
 	echo "$processes, $size, $result_bcast"
         # Scrivi i risultati nel file CSV
-        echo "$processes,$size,$result_bcast" >> output_processes_size_2_new.csv
+        echo "$processes,$size,$result_bcast" >> output_processes_size_gather_2_new.csv
     done
 done
