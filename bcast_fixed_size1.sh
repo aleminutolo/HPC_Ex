7e679d0fbba9b0bed28ba38fe56b1e5bed181747
Alessandro Minutolo <minutoloalessandro@gmail.com>
2024-02-17 13:45:18 +0000

    m

--- bcast_fixed_size1.sh
+++ bcast_fixed_size1.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=HPC
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=128
 #SBATCH --time=02:00:00
-#SBATCH --partition THIN
+#SBATCH --partition EPYC
 #SBATCH --exclusive
 #SBATCH --exclude fat[001-002]
 
 module load openMPI/4.1.5/gnu/12.2.1
 
-echo "Processes,Latency" > output_processes_fixed_size_bcast_1_new.csv
+echo "Processes,Latency" > output_processes_epyc_fixed_size_bcast_1_new.csv
 
 # Numero di ripetizioni per ottenere una media
 repetitions=10000
 echo "size = 2"
 # Ciclo esterno per il numero di processori
-for processes in {2..48..2}
-do
+for i in {1..8}
+do processes = $(2**i)
   
   # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
   result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 1 osu_bcast -m 2 -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
 	
 	echo "$processes, $result_bcast"
         # Scrivi i risultati nel file CSV
-        echo "$processes,$size,$result_bcast" >> output_processes_fixed_size_bcast_1_new.csv
- done
+        echo "$processes,$size,$result_bcast" >> output_processes_epyc_fixed_size_bcast_1_new.csv
+done
