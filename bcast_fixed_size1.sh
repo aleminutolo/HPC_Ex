728e8316e819a028d674467d7483bffaa46831ff
Alessandro Minutolo <minutoloalessandro@gmail.com>
2024-02-19 15:18:39 +0000

    m

--- bcast_fixed_size1.sh
+++ bcast_fixed_size1.sh
@@ -16,7 +16,7 @@ repetitions=10000
 echo "size = 2"
 # Ciclo esterno per il numero di processori
 for i in {1..8}
-do processes = $(2**i)
+do processes = $((2**i))
   
   # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
   result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 1 osu_bcast -m 2 -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
