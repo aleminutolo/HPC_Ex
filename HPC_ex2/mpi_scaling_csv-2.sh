9412374a2bb7a4f50ed0fb9708b500255da24630
Alessandro Minutolo <98023987+aleminutolo@users.noreply.github.com>
2024-04-08 17:46:23 +0200

    Add files via upload

---
+++ HPC_ex2/mpi_scaling_csv-2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=HPC-MPI-Scaling
+#SBATCH --nodes=4  # Massimo numero di nodi che vuoi testare
+#SBATCH --ntasks-per-node=24  # Adatta questo al massimo numero di task per nodo
+#SBATCH --time=02:00:00
+#SBATCH --partition=THIN
+#SBATCH --exclusive
+
+module load openMPI/4.1.5/gnu/12.2.1
+
+export OMP_NUM_THREADS=1
+
+# Nome del file CSV per salvare i tempi di esecuzione
+output_file="execution_times_mpi.csv"
+
+# Creiamo l'intestazione del file CSV
+echo "Total Tasks,Execution Time (s)" > $output_file
+
+# Loop attraverso un numero variabile di task MPI totali
+for total_tasks in {2..96..2}; do
+    echo "Running with $total_tasks MPI tasks."
+    
+    # Esegui mpirun e cattura il tempo di esecuzione
+    mpirun -np $total_tasks ./mandelbrot 800 600 -2.0 -1.0 1.0 1.0 255
+    
+    # Aggiungi i dati al file CSV
+    execution_time=$(<temp_execution_time.txt)
+    echo "$total_tasks,$execution_time" >> $output_file
+    rm temp_execution_time.txt # Rimuovi il file temporaneo
+done
+
---
+++ HPC_ex2/omp_weak_scaling.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=HPC-OMP-WeakScaling
+#SBATCH --nodes=1  # Utilizza un singolo nodo
+#SBATCH --ntasks-per-node=1  # Un solo task per nodo
+#SBATCH --cpus-per-task=24  # Massimo numero di thread OMP che vuoi testare
+#SBATCH --time=02:00:00
+#SBATCH --partition=THIN
+#SBATCH --exclusive
+
+module load openMPI/4.1.5/gnu/12.2.1
+
+# Definisci il nome del file CSV per salvare i risultati
+OUTPUT_CSV="weak_scaling_omp.csv"
+
+# Inizializza il file CSV con l'intestazione
+echo "OMP_NUM_THREADS,Problem_Size,Execution_Time" > $OUTPUT_CSV
+# Dimensione di base del problema per 1 thread OpenMP
+BASE_COLS=800
+BASE_ROWS=600
+
+for OMP_NUM_THREADS in {1..24}; do
+    export OMP_NUM_THREADS
+    # Aumenta la dimensione del problema proporzionalmente al numero di thread
+    let cols=BASE_COLS*OMP_NUM_THREADS
+    let rows=BASE_ROWS*OMP_NUM_THREADS
+    # Esegui il programma e salva il tempo di esecuzione
+    mpirun ./mandelbrot $cols $rows -2.0 -1.0 1.0 1.0 255
+    
+    execution_time=$(<temp_execution_time.txt)
+    echo "$OMP_NUM_THREADS,$cols,$rows,$execution_time" >> $OUTPUT_CSV
+    rm temp_execution_time.txt # Rimuovi il file temporaneo
+
+    
+done
