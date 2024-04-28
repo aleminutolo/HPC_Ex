3be848593bd2ac9804d1b6ac31aff144b6662be2
aleminutolo <minutoloalessandro@gmail.com>
2024-04-28 15:26:35 +0000

    m

--- HPC_ex1/gather2_node_thin.sh
+++ HPC_ex1/gather2_node_thin.sh
@@ -9,13 +9,13 @@
 
 module load openMPI/4.1.5/gnu/12.2.1
 
-echo "Processes,Size,Latency" > gather0_core_thin_part2.csv
+echo "Processes,Size,Latency" > gather2_core_thin.csv
 
 # Numero di ripetizioni per ottenere una media
 repetitions=10000
 
 # Ciclo esterno per il numero di processori
-for processes in {36..48..2}
+for processes in {2..48..2}
 do
     # Ciclo interno per la dimensione del messaggio da 2^1 a 2^20
     for size_power in {1..20}
@@ -24,10 +24,10 @@ do
         size=$((2**size_power))
 
         # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-        result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_gather_algorithm 0 osu_gather -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
-	
-	echo "$processes, $size, $result_bcast"
+        result_bcast=$(mpirun --map-by node -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_gather_algorithm 2 osu_gather -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
+
+        echo "$processes, $size, $result_bcast"
         # Scrivi i risultati nel file CSV
-        echo "$processes,$size,$result_bcast" >> gather0_core_thin_part2.csv
+        echo "$processes,$size,$result_bcast" >> gather2_node_thin.csv
     done
 done
