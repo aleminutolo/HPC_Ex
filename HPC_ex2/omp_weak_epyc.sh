7d2ff511651227740254f94538e12d6b0e99cbd8
aleminutolo <minutoloalessandro@gmail.com>
2024-05-03 09:00:18 +0000

    dati_penso_giusti

--- HPC_ex2/omp_weak_epyc.sh
+++ HPC_ex2/omp_weak_epyc.sh
@@ -9,30 +9,29 @@
 
 module load openMPI/4.1.5/gnu/12.2.1
 
+export OMP_PROC_BIND=spread
+export OMP_PLACES=cores
+export OMP_DISPLAY_ENV=true
+export OMP_VERBOSE=VERBOSE
+
 # Definisci il nome del file CSV per salvare i risultati
 OUTPUT_CSV="omp_weak_epyc.csv"
 
 # Inizializza il file CSV con l'intestazione
 echo "OMP_NUM_THREADS,Problem_Size,Execution_Time" > $OUTPUT_CSV
 # Dimensione di base del problema per 1 thread OpenMP
-BASE_COLS=800
-BASE_ROWS=600
+BASE_COLS=1000
+BASE_ROWS=1000
 
-for OMP_NUM_THREADS in {2..128..2}; do
+for OMP_NUM_THREADS in {1..128}; do
     export OMP_NUM_THREADS
-    # Aumenta la dimensione del problema proporzionalmente alla radice quadrata del numero di thread
-    let cols=$(echo "scale=0; sqrt($OMP_NUM_THREADS) * $BASE_COLS" | bc)
-    let rows=$(echo "scale=0; sqrt($OMP_NUM_THREADS) * $BASE_ROWS" | bc)
-
+    # Aumenta la dimensione del problema proporzionalmente al numero di thread
+    let cols=$BASE_COLS
+    let rows=$((BASE_ROWS*OMP_NUM_THREADS))
     # Esegui il programma e salva il tempo di esecuzione
-    mpirun --map-by socket --bind-to core ./mandel $cols $rows -2.0 -1.0 1.0 1.0 255 $OMP_NUM_THREADS
+    execution_time=$(mpirun -np 1 --map-by socket --bind-to socket ./mandel $cols $rows -2.0 -1.0 1.0 1.0 255 $OMP_NUM_THREADS)
 
-    if [ -f temp_execution_time.txt ]; then
-        execution_time=$(<temp_execution_time.txt)
-        echo "$OMP_NUM_THREADS,$cols*$rows,$execution_time" >> $OUTPUT_CSV
-        rm temp_execution_time.txt # Rimuovi il file temporaneo
-    else
-        echo "$OMP_NUM_THREADS,$cols*$rows,FAILED" >> $OUTPUT_CSV
-    fi
+    echo "$OMP_NUM_THREADS,$cols,$rows,$execution_time" >> $OUTPUT_CSV
+
+    
 done
-
